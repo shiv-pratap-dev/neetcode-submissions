a518cf8c4a1142389d56d152aec0ed46f9941180
shiv-pratap-dev <shivytsshiv@gmail.com>
2026-06-11 15:26:58 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        isPerm = False
+        for i in range(len(s2) - len(s1) +1):
+            if sorted(list(s2[i: i+len(s1)])) == sorted(list(s1)):
+                isPerm = True
+                break
+            else:
+                isPerm = False
+                
+     
+        return (isPerm)  
+        
